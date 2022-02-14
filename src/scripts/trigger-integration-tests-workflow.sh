36798b701cb5ed005d0e9e021e0a06ea9c79044f
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-02-14 09:48:46 -0800

    add emoji

--- src/scripts/trigger-integration-tests-workflow.sh
+++ src/scripts/trigger-integration-tests-workflow.sh
@@ -21,6 +21,7 @@ Result() {
         exit 1
     else
         echo "Pipeline triggered!"
+        echo "👋"
         echo "${CIRCLECI_APP_HOST}/pipelines/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/$(cat /tmp/curl-result.txt | jq -r .number)"
     fi
 }
